892e39f460736e220c7161cada04361eacca5c74
furkangokcee <furkangokce040@gmail.com>
2025-12-10 20:31:15 +0300

    Kus hareketi ve animasyonu eklendi

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    // Uçağın ne kadar güçlü zıplayacağını buradan ayarlayacağız.
+    // [SerializeField] yazmamızın sebebi: Unity editöründen bu sayıyı değiştirebilmek.
+    [SerializeField] private float jumpForce = 5f;
+
+    // Fizik motoruna erişmek için bir değişken.
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        // Oyun başlarken uçağın üzerindeki Rigidbody 2D bileşenini bulup hafızaya alıyoruz.
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Eğer Mouse'un sol tuşuna basılırsa (Telefonda dokunmaya denk gelir)
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            // Mevcut hızı sıfırla (yoksa düşerken basarsan yukarı çıkması zorlaşır)
+            // Sonra yukarı doğru kuvvet uygula (Vector2.up = (0, 1))
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
